d8c954bd63061ffd327f4d69ab2691419f01eff2
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-08-01 18:29:13 +0300

    postfix in build input

--- default.nix
+++ default.nix
@@ -142,6 +142,7 @@ sh = dash.overrideAttrs (_: rec {
       doCheck = false;
       checkTarget = "test";
       buildInputs = [
+         postfix
          autoconf213
          automake
          pkgconfig
