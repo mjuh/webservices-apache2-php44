21d43d184270b142dd40197098e2b6945857e458
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-12-16 19:22:42 +0300

    add tests

---
+++ test.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> {
+  overlays = [
+    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "refactor"; }))
+  ];
+};
+
+maketestPhp {
+  php = php4;
+  image = callPackage ./default.nix {};
+  rootfs = ./rootfs;
+}
