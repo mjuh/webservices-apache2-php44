008c473e541d0cf4e06ff5bc9db4ce50a1d6cb2d
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-12-18 14:54:37 +0300

    add JenkinsJobUrl and GitlabCommitUrl

--- default.nix
+++ default.nix
@@ -56,8 +56,9 @@ dockerArgHints = {
 
 gitAbbrev = firstNChars 8 (getEnv "GIT_COMMIT");
 gitCommit = (getEnv "GIT_COMMIT");
-jenkinsJobUrl = (getEnv "JOB_URL");
-
+jenkinsBuildUrl = (getEnv "BUILD_URL");
+jenkinsJobName = (getEnv "JOB_NAME");
+gitlabCommitUrl = "https://gitlab.intr/" + jenkinsJobName + "/commit/" + gitCommit;
 in 
 
 pkgs.dockerTools.buildLayeredImage rec {
@@ -96,8 +97,8 @@ pkgs.dockerTools.buildLayeredImage rec {
       ru.majordomo.docker.arg-hints-json = builtins.toJSON dockerArgHints;
       ru.majordomo.docker.cmd = dockerRunCmd dockerArgHints "${name}:${tag}";
       ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
-      ru.majordomo.ci.jenkins.job_url = if jenkinsJobUrl != "" then jenkinsJobUrl else "none";
-#      ru.majordomo.ci.gitlab.commit_url = "https://gitlab.intr/(builtins.getEnv "JOB_NAME")/commit/(builtins.getEnv "GIT_COMMIT")";
+      ru.majordomo.ci.jenkins.build.url = if jenkinsBuildUrl != "" then jenkinsBuildUrl else "none";
+      ru.majordomo.ci.gitlab.commit.url = if gitlabCommitUrl != "" then gitlabCommitUrl else "none";
     };
   };
     extraCommands = ''
