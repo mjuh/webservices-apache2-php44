ab7f1512d7139ac49503284005bb927e4d6a40d4
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-21 19:19:36 +0300

    Revert "add some ci labels"

--- default.nix
+++ default.nix
@@ -54,12 +54,6 @@ dockerArgHints = {
     ];
   };
 
-gitAbbrev = firstNChars 8 (getEnv "GIT_COMMIT");
-gitCommit = (getEnv "GIT_COMMIT");
-jenkinsBuildUrl = (getEnv "BUILD_URL");
-jenkinsJobName = (getEnv "JOB_NAME");
-jenkinsBranchName = (getEnv "BRANCH_NAME");
-gitlabCommitUrl = "https://gitlab.intr/" + (replaceStrings [jenkinsBranchName ""] ["" ""] jenkinsJobName) + "/commit/" + gitCommit;
 in 
 
 pkgs.dockerTools.buildLayeredImage rec {
@@ -98,8 +92,6 @@ pkgs.dockerTools.buildLayeredImage rec {
       ru.majordomo.docker.arg-hints-json = builtins.toJSON dockerArgHints;
       ru.majordomo.docker.cmd = dockerRunCmd dockerArgHints "${name}:${tag}";
       ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
-      ru.majordomo.ci.jenkins.build.url = if jenkinsBuildUrl != "" then jenkinsBuildUrl + "console" else "none";
-      ru.majordomo.ci.gitlab.commit.url = if gitlabCommitUrl != "" then gitlabCommitUrl else "none";
     };
   };
     extraCommands = ''
